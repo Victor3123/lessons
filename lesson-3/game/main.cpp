2398a9b38c89e38b78bfe71b8fce4aae52e541f4
Victor3123 <78618641+Victor3123@users.noreply.github.com>
2021-04-21 14:36:27 +0300

    added edit code in project game

--- lesson-3/game/main.cpp
+++ lesson-3/game/main.cpp
@@ -5,16 +5,12 @@
 
 using namespace std;
 
-int main() {
 
-    SetConsoleOutputCP(CP_UTF8);
-
+void game(){
     int comp;
     int igrok;
     time_t t;
-    char dp;
 
-    g:
     srand((unsigned) time(&t));
     comp = rand() % 6 + 1;
 
@@ -37,17 +33,44 @@ int main() {
         cout << "Вы проиграли" << endl;
 
     }
+}
 
+int replay(char dp){
     cout << "Хотите сыграть еще раз? Y/N" << endl;
     cin >> dp;
     if (dp == 'Y' || dp == 'y') {
+        return 1;
+    }
+    else if (dp == 'N' || dp == 'n') {
+        return 2;
+    }
+    else {
+        return 3;
+    }
+//    else {
+//        cout << "Вы ввели неверные данные\n";
+//        goto c;
+//    }
+
+}
+
+int main() {
+    char dp;
+    SetConsoleOutputCP(CP_UTF8);
+
+    g:
+    game();
+    r:
+    int a = replay(dp);
+    if(a == 1){
         goto g;
     }
-
-    if (dp == 'N' || dp == 'n') {
-        return (0);
+    else if(a == 2){
+        return 0;
     }
-
+    else if(a == 3){
+        goto r;
+    }
 
     cin.get();
     cin.get();
