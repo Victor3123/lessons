b2e908c851a63ffab53bcf0cdc351768601f2834
Victor3123 <78618641+Victor3123@users.noreply.github.com>
2021-04-28 15:02:17 +0300

    lesson 4

---
+++ lesson-4/task-3-on-cpp/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+
+int main() {
+    srand(time(0));
+    const int DIM1 = 100;
+    const int DIM2 = 100;
+
+    int ary[DIM1][DIM2];
+    int o = 100;
+
+
+    for (int i = 0; i < DIM1; i++) {
+        for (int j = 0; j < DIM2; j++) {
+            ary[i][j] = rand() % 90 + 10;
+        }
+    }
+    for (int i = 0; i < DIM1; i++) {
+        for (int j = 0; j < DIM2; j++) {
+            cout << " " << ary[i][j];
+            if (ary[i][j] < o && ary[i][j] % 2 == 0){
+                o = ary[i][j];
+            }
+        }
+        cout << endl;
+    }
+    cout << "====================================================================================\n" << o;
+    return 0;
+}
